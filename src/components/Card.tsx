f7592f7eb18181488dd812bd2dcb0873e1cf13ca
장현웅 <jhw811@gmail.com>
2022-01-09 02:29:10 +0900

    카드애니메이션 추가

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,35 +1,35 @@
-import {MouseEvent, useContext} from "react";
-import {GlobalContext} from "../GlobalContext";
-import {CardStatus} from "../types";
+import { MouseEvent, useContext, useState } from 'react';
+import { GlobalContext } from '../GlobalContext';
+import Bad from '../images/bad.png';
+import Good from '../images/good.png';
+import { CardStatus } from '../types';
 
 interface Props {
   status: CardStatus;
 }
 
-export default function Card({status}: Props) {
+export default function Card({ status }: Props) {
   const context = useContext(GlobalContext);
-  const onClickCard = ({currentTarget}: MouseEvent<HTMLButtonElement>) => {
+  const [open, isOpen] = useState(false);
+  const onClickCard = ({ currentTarget }: MouseEvent<HTMLButtonElement>) => {
     const {
-      dataset: {status},
+      dataset: { status },
     } = currentTarget;
 
     if (!status) {
       return;
     }
-
-    currentTarget.classList.remove("unselected");
-    currentTarget.classList.add("fliped");
+    isOpen(true);
 
     context?.caculateTotal(status as CardStatus);
   };
 
   return (
-    <button
-      className="card unselected"
-      data-status={status}
-      onClick={onClickCard}
-    >
-      {status === "sad" ? "🤢" : "😊"}
+    <button className={`card ${open ? 'fliped' : ''}`} data-status={status} onClick={onClickCard}>
+      <div className={`back ${open ? 'open' : ''}`}></div>
+      <div className={`front ${open ? 'open' : ''} ${status === 'sad' ? 'bad' : 'good'}`}>
+        {<img src={status === 'sad' ? Bad : Good} alt='' />}
+      </div>
     </button>
   );
 }
